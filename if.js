ff12485de3f052d19c325ed96628769f1e5d6833
Peter Ndungu <93506710+petreleven@users.noreply.github.com>
2026-03-25 17:31:16 +0300

    Create if.js

---
+++ if.js
@@ -0,0 +1,38 @@
+
+
+## 🏎️ Mission: The Getaway Van
+**The Mission:** Your squad found an old van, but you need to check the fuel gauge before you hit the gas! Create a variable called `fuelLevel` and set it to **45**. 
+
+Write an `if / else if / else` statement that checks the fuel:
+* **If** `fuelLevel` is more than **80**: Print "Full tank! Let's drive to the Secret Base."
+* **Else if** `fuelLevel` is more than **20**: Print "Running low. Stop at the gas station!"
+* **Else**: Print "Empty! Grab your running shoes!"
+
+**Check your work:** Change the `fuelLevel` number a few times to make sure your code reacts correctly to every situation!
+
+---
+
+## 🤖 Mission: Robot's Combat Mode
+**The Mission:** Your "Robot" teammate has three modes depending on how many zombies are approaching. Create a variable called `zombieCount` and set it to **12**.
+
+Write a conditional statement for the Robot:
+* **If** `zombieCount` is **0**: Print "Scanning... all clear!"
+* **Else if** `zombieCount` is less than **10**: Print "Target locked. Initializing Laser Beam."
+* **Else**: Print "Too many zombies! Deploying Smoke Bomb!"
+
+**Check your work:** Print the result to the console. Does the Robot know when to fight and when to hide?
+
+---
+
+## 🍕 Mission: The Pizza Guy's Choice
+**The Mission:** The "Pizza Guy" found a mysterious crate! He needs to decide what to do based on the color of the crate. Create a variable called `crateColor` and set it to **"Red"**.
+
+Write the logic to open it:
+* **If** the color is **"Gold"**: Print "You found a Legendary Sword!"
+* **Else if** the color is **"Silver"**: Print "You found a Med-Kit!"
+* **Else**: Print "It's just a box of stale pepperoni. Yuck."
+
+**Check your work:** Make sure you use `==` or `===` to compare the strings. Try changing the color to "Gold" to see if you get the sword!
+
+---
+
